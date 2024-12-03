31df2b7c8bfed73bae4e7741950ea123bb90bf55
elias <elias@intuicell.com>
2024-12-03 20:06:08 +0100

    Day 3 part 1

--- day3/day3.py
+++ day3/day3.py
@@ -1,5 +1,13 @@
 import re
 
+## Part 1
+inp = open("input.txt").read()
+matches = re.findall(r"(?<=mul\()(\d+),(\d+)(?=\))", inp)
+
+part1 = sum(int(x) * int(y) for x, y in matches)
+print(f"{part1=}")
+
+## Part 2
 inp = open("input.txt").read()
 matches = re.findall(r"(?<=mul\()(\d+),(\d+)(?=\))", inp)
 
