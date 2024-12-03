76de2d609018015853186ead329d8de8b883d937
elias <elias.aronson@gmail.com>
2024-12-03 21:11:32 +0100

    Day 3 part 2

--- day3/day3.py
+++ day3/day3.py
@@ -9,7 +9,30 @@ print(f"{part1=}")
 
 ## Part 2
 inp = open("input.txt").read()
+
+dos = [0] + [x.span()[1] for x in re.finditer(r"do\(\)", inp)]
+donts = [x.span()[1] for x in re.finditer(r"don't\(\)", inp)] + [len(inp)]
+
+ranges = []
+for do in dos:
+    for dont in donts:
+        if dont > do:
+            ranges.append((do, dont))
+            break
+
+
+def in_range(ranges, i):
+    for range in ranges:
+        if range[0] < i and i < range[1]:
+            return True
+    return False
+
+
 matches = re.findall(r"(?<=mul\()(\d+),(\d+)(?=\))", inp)
 
-part1 = sum(int(x) * int(y) for x, y in matches)
-print(f"{part1=}")
+part2 = 0
+for mul in re.finditer(r"(?<=mul\()(\d+),(\d+)(?=\))", inp):
+    if in_range(ranges, mul.span()[1]):
+        part2 += int(mul.groups()[0]) * int(mul.groups()[1])
+
+print(f"{part2=}")
